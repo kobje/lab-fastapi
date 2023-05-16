0eeea661d6943b9915ec33de331888e418c4df21
kobje <fjm.kos@gmail.com>
2023-05-16 09:28:06 +0200

    commit2

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/students")
+async def createStudent(student: StudentCreateSchema):
+    return student
