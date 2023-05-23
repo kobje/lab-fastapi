5a80db67c5e37fe38af88f66596e9799690c8e77
kobje <fjm.kos@gmail.com>
2023-05-23 09:10:09 +0200

    commit4

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class StudentCreateSchema(BaseModel):
+    student_id: int
+    first_name: str
+    last_name: str
+
+class StudentUpdateSchema(BaseModel):
     first_name: str
     last_name: str
 
@@ -16,3 +21,14 @@ async def createStudent(student: StudentCreateSchema):
 @app.get("/")
 async def root():
     return students
+@app.get("/student/<id>")
+async def get_student(id: int):
+    for student in students:
+        if student.student_id == id:
+            return student
+@app.post("/student/<id>")
+async def update_student(id: int, updated_student: StudentUpdateSchema):
+    for student in students:
+        if student.student_id == id:
+            student.first_name = updated_student.first_name
+            student.last_name = updated_student.last_name
