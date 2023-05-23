d7d1dc78a5d954a8a0d5971d4146617d7eec4aa5
kobje <fjm.kos@gmail.com>
2023-05-23 08:23:27 +0200

    commit3

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class StudentCreateSchema(BaseModel):
     first_name: str
     last_name: str
 
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+students = []
 
 @app.post("/students")
 async def createStudent(student: StudentCreateSchema):
+    students.append(student)
     return student
+@app.get("/")
+async def root():
+    return students
